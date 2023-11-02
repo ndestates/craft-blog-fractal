091012ad27cb750b86dde373f9e5a9b2c457de13
ndestates <nick@ndestates.com>
2023-11-02 16:43:30 +0000

    Modified Tailwind

--- craft-blog-fractal/tailwind.config.js
+++ craft-blog-fractal/tailwind.config.js
@@ -1,42 +1,42 @@
 module.exports = {
-  purge: [],
+  theme: {
+    extend: {},
+  },
   theme: {
     typography: {
       default: {
         css: {
-          color: '#333',
+          color: "#333",
           a: {
-            'text-decoration': 'none',
-            color: '#000000',
-            '&:hover': {
-              color: '#2c5282',
+            "text-decoration": "none",
+            color: "#000000",
+            "&:hover": {
+              color: "#2c5282",
             },
           },
-          'article-title': {
-            'font-weight': 100,
-            'margin-bottom': 0,
+          "article-title": {
+            "font-weight": 100,
+            "margin-bottom": 0,
           },
         },
       },
     },
     fontFamily: {
-      'script': ['Pacifico'],
-      body: ['Merriweather Sans', 'sans-serif'],
+      script: ["Pacifico"],
+      body: ["Merriweather Sans", "sans-serif"],
     },
     extend: {
       spacing: {
-        headerbgtall: '300px',
-        headbgshort: '150px',
-        overlaybg: '200px',
+        headerbgtall: "300px",
+        headbgshort: "150px",
+        overlaybg: "200px",
       },
-      backgroundImage: theme => ({
-        'coffee-shop-bg': "url('/img/coffee-shop.jpg')",
-        'image-with-overlay': "url('/img/espresso-shot.jpg')",
-      })
+      backgroundImage: (theme) => ({
+        "coffee-shop-bg": "url('/img/coffee-shop.jpg')",
+        "image-with-overlay": "url('/img/espresso-shot.jpg')",
+      }),
     },
   },
   variants: {},
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
